53cf26c2a9fa2ec13263eadd50e0641dc25d1eed
tomas rechtig <tomas.rechtig@7interactive.cz>
2017-01-29 19:44:54 +0100

    Rekurzivni rozprskavani

--- app/src/main/java/com/example/semanticer/unstable/domain/GameImpl.java
+++ app/src/main/java/com/example/semanticer/unstable/domain/GameImpl.java
@@ -23,12 +23,8 @@ public class GameImpl implements Game {
     }
 
     private GameImpl(int rowCount, int columnCount) {
-
-        List<List<GameField>> fields = getFields();
-
-        playerOnTurn = Player.FIRST_PLAYER;
-
-        gameBoard = GameBoard.create(fields);
+        playerOnTurn = Player.ANON;
+        gameBoard = GameBoard.create(getFields(rowCount, columnCount));
     }
 
 
@@ -42,32 +38,42 @@ public class GameImpl implements Game {
             throw new IllegalStateException("Impossible to make move to position x: " + x + " y: " + y);
         }
 
-        switch (gameBoard.fields().get(x).get(y).atomCount()){
-            case 1:
-                gameBoard.fields().get(x).set(y, GameField.create(2, playerOnTurn));
-                break;
-            case 2:
-                gameBoard.fields().get(x).set(y, GameField.create(3, playerOnTurn));
-                break;
-            case 3:
-                gameBoard.fields().get(x).set(y, GameField.create(1, playerOnTurn));
-                break;
-            default:
-                gameBoard.fields().get(x).set(y, GameField.create(1, playerOnTurn));
-                break;
+        switchPlayerOnTurn();
+
+        return resolveTurn(x, y);
+    }
+
+    private GameBoard resolveTurn(int x, int y){
+
+        if(gameBoard.fields().get(x).get(y).atomCount() < 3){
+            gameBoard.fields().get(x).set(y, GameField.create(gameBoard.fields().get(x).get(y).atomCount() + 1, playerOnTurn));
+        } else {
+            System.out.println("----------------------------------------------");
+            gameBoard.fields().get(x).set(y, GameField.createBlank());
+            if (x > 0){
+                System.out.println("# x > 0   x: " + x + " y: " + y);
+                resolveTurn(x - 1, y);
+            }
+            if (x < gameBoard.rows() - 1){
+                System.out.println("# x < " + gameBoard.rows() + "   x: " + x + " y: " + y);
+                resolveTurn(x + 1, y);
+            }
+            if (y > 0) {
+                System.out.println("# y > 0   x: " + x + " y: " + y);
+                resolveTurn(x, y - 1);
+            }
+            if (y < gameBoard.columns() - 1){
+                System.out.println("# y < " + gameBoard.columns() + "   x: " + x + " y: " + y);
+                resolveTurn(x, y + 1);
+            }
         }
 
-        switchPlayerOnTurn();
         return gameBoard;
     }
 
     @Override
     public boolean isMovePossible(int x, int y) {
-        // TODO return if
-
-        if (gameBoard.fields().get(x).get(y).player() == playerOnTurn || gameBoard.fields().get(x).get(y).player() == Player.ANON) return true;
-
-        return false;
+        return gameBoard.fields().get(x).get(y).player() != playerOnTurn || gameBoard.fields().get(x).get(y).player() == Player.ANON;
     }
 
     @Override
@@ -75,16 +81,16 @@ public class GameImpl implements Game {
         return gameBoard;
     }
 
+    private List<List<GameField>> getFields(int rows, int columns) {
+        List<List<GameField>> board = new ArrayList<>();
 
-    // TODO remove this example method
-    public List<List<GameField>> getFields() {
-        return Arrays.asList(
-                Arrays.asList(GameField.createBlank(), GameField.createBlank(), GameField.createBlank(), GameField.createBlank()),
-                Arrays.asList(GameField.createBlank(), GameField.createBlank(), GameField.createBlank(), GameField.createBlank()),
-                Arrays.asList(GameField.createBlank(), GameField.createBlank(), GameField.createBlank(), GameField.createBlank()),
-                Arrays.asList(GameField.createBlank(), GameField.createBlank(), GameField.createBlank(), GameField.createBlank()),
-                Arrays.asList(GameField.createBlank(), GameField.createBlank(), GameField.createBlank(), GameField.createBlank()),
-                Arrays.asList(GameField.createBlank(), GameField.createBlank(), GameField.createBlank(), GameField.createBlank())
-        );
+        for (int i = 0; i < rows; i++) {
+            board.add(i, new ArrayList<>());
+            for (int j = 0; j < columns; j++) {
+                board.get(i).add(j, GameField.createBlank());
+            }
+        }
+
+        return board;
     }
 }
